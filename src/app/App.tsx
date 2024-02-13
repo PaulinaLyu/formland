61e4ccf3acd48937e3df3424601ba0156ff7f686
Polina Lyubeznova <polina.lyubeznova@geogracom.com>
2024-02-13 15:34:19 +0300

    feature(app/App): Добавила Lazy подгрузку страниц и Suspense

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,31 +1,103 @@
-import { FormConstructorPage } from "@/pages/FormConstructorPage";
+import { Suspense, lazy } from "react";
+import { NavLink, Route, Routes } from "react-router-dom";
 import { FormsListPage } from "@/pages/FormListPage";
-import { FormPage } from "@/pages/FormPage";
-import { FormResponsesPage } from "@/pages/FormResponsesPage";
-import { LoginPage } from "@/pages/LoginPage";
-import { OwnProfilePage } from "@/pages/OwnProfilePage";
-import { ResponsePage } from "@/pages/ResponsePage";
-import { SignupPage } from "@/pages/SignupPage";
 
-import { Route, Routes } from "react-router-dom";
+const OwnProfilePage = lazy(() =>
+  import("@/pages/OwnProfilePage").then((module) => ({
+    default: module.OwnProfilePage,
+  }))
+);
+
+const ResponsePage = lazy(() =>
+  import("@/pages/ResponsePage").then((module) => ({
+    default: module.ResponsePage,
+  }))
+);
+
+const FormResponsesPage = lazy(() =>
+  import("@/pages/FormResponsesPage").then((module) => ({
+    default: module.FormResponsesPage,
+  }))
+);
+
+const FormPage = lazy(() =>
+  import("@/pages/FormPage").then((module) => ({
+    default: module.FormPage,
+  }))
+);
+
+const FormConstructorPage = lazy(() =>
+  import("@/pages/FormConstructorPage").then((module) => ({
+    default: module.FormConstructorPage,
+  }))
+);
+
+const LoginPage = lazy(() =>
+  import("@/pages/LoginPage").then((module) => ({
+    default: module.LoginPage,
+  }))
+);
+
+const SignupPage = lazy(() =>
+  import("@/pages/SignupPage").then((module) => ({
+    default: module.SignupPage,
+  }))
+);
+
+const NotFoundPage = lazy(() =>
+  import("@/pages/NotFoundPage").then((module) => ({
+    default: module.NotFoundPage,
+  }))
+);
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<FormsListPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/me" element={<OwnProfilePage />} />
-      <Route path="/forms/edit" element={<FormConstructorPage />} />
-      <Route path="/forms/new" element={<FormConstructorPage />} />
-      <Route path="/forms/:formId" element={<FormPage />} />
-      <Route path="/forms/:formId/responses" element={<FormResponsesPage />} />
-      <Route path="/forms/:formId/responses/:responseId" element={<ResponsePage />} />
-      {/* <Route path="profile" element={<ProfileLayout />}>
-        <Route path="me" element={<OwnUserProfile />} />
-        <Route path=":id" element={<UserProfile />} />
-      </Route> */}
-    </Routes>
+    <>
+      <nav>
+        <NavLink
+          style={{ marginRight: "10px" }}
+          replace
+          to="/login"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >
+          Login
+        </NavLink>
+
+        <NavLink
+          to="/signup"
+          replace
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >
+          signup
+        </NavLink>
+      </nav>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<FormsListPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/me" element={<OwnProfilePage />} />
+
+          {/* <Route path="/forms/*" element={<FormsRoutes />} /> */}
+
+          <Route path="/forms">
+            <Route path="edit" element={<FormConstructorPage />} />
+            <Route path="new" element={<FormConstructorPage />} />
+            <Route path=":formId" element={<FormPage />} />
+            <Route path="responses">
+              <Route index element={<FormResponsesPage />} />
+              <Route path=":responseId" element={<ResponsePage />} />
+            </Route>
+          </Route>
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 }
 
